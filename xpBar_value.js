4b589cd5177f9846ce295fbed1d97f0db81f26c2
ChromaTechFR <45916472+ChromaTechFR@users.noreply.github.com>
2020-09-18 23:01:21 +0200

    progressBar function.

---
+++ xpBar_value.js
@@ -0,0 +1,17 @@
+const Jimp = require("jimp");
+
+const xpBar = (hexColor, currentXP, xpNeeded) => {
+    return new Promise(resolve => {
+        new Jimp(300, 10,"000000", async (err, image) => {
+            if (err) throw err;
+            const bar = currentXP / xpNeeded * 300;
+            new Jimp(bar, 10, hexColor, async (err, color) => {
+                if (err) console.error(err);
+                image.blit(color, 0, 0);
+                image.composite(color, 0, 0);
+                const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+                resolve(buffer);
+            });
+        });
+    });
+}
